1c5a3568431dbb272621a9c4b118b408c4ce0493
T3gharee <t3gharee@uwaterloo.ca>
2020-12-30 17:11:36 -0500

    Update VideoText.py

--- VideoText.py
+++ VideoText.py
@@ -4,6 +4,7 @@ Created on Wed Dec 30 16:56:16 2020
 
 @author: tghareeb
 """
+# Print frame size and date
 
 import cv2
 import datetime
@@ -14,7 +15,7 @@ print(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 #cap.set(4, 3000)
 #print(cap.get(3))
 #print(cap.get(4))
-while(cap.isOpened()):
+while(cap.isOpened()):       #  # OR while(true) or while (1) 
     ret, frame = cap.read()
     if ret == True:
 
@@ -22,15 +23,15 @@ while(cap.isOpened()):
        text = '(Width: '+ str(cap.get(3)) + ' Height:' + str(cap.get(4))+')'
        datet = str(datetime.datetime.now())
        
-       frame = cv2.putText(frame, text, (10, 50), font, 0.5,
+       frame = cv2.putText(frame, text, (10, 30), font, 0.5,
                            (0, 255, 255), 1, cv2.LINE_AA)
       
-       frame = cv2.putText(frame, datet, (10, 80), font, 0.5,
+       frame = cv2.putText(frame, datet, (10, 60), font, 0.5,
                            (0, 255, 0), 1, cv2.LINE_AA)
       
-       frame = cv2.resize(frame,(960, 720), interpolation = cv2.INTER_CUBIC)
+       # frame = cv2.resize(frame,(960, 720), interpolation = cv2.INTER_CUBIC)
 
-       cv2.imshow('frame', frame)
+       cv2.imshow('Frame Info', frame)
 
        if cv2.waitKey(1) & 0xFF == 27:
        # if cv2.waitKey(1) & 0xFF == ord('q'):
